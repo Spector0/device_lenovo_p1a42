fe8792a3ca7d2e5abfb3d81ceee92209476a0728
Spector <33796688+Spector0@users.noreply.github.com>
2017-12-02 22:28:43 +0530

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -26,9 +26,9 @@ INITIAL_COPYRIGHT_YEAR=2016
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -52,7 +52,10 @@ else
 fi
 
 # Initialize the helper
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false $clean_vendor
++
++extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+  
     extract "$MY_DIR"/proprietary-files.txt "$SRC"
     INITIAL_COPYRIGHT_YEAR=2016
 
